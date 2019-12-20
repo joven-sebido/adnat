2c82a8ec69d2fbdfefaabe956f46c08aea25252f
Joven Sebido <jvn.sbd@gmail.com>
2019-12-20 17:36:15 +0000

    Created User model and sign-up process

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
   root 'application#hello'
+  get '/home', to: 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+  get '/login',  to: 'static_pages#home'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
